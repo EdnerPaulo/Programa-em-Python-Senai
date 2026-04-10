0391f9e5009360a3ccfb37af508e7f96277bff50
Edner Paulo Severo Dias <163218158+EdnerPaulo@users.noreply.github.com>
2026-04-09 21:21:04 -0300

    Add files via upload

---
+++ aula 7/compras.py
@@ -0,0 +1,50 @@
+produtos = {
+    "Nada": 0.00,
+    "Livro": 50.0,
+    "Tablet": 250.0,
+    "Fones de Ouvido": 15.0
+}
+carrinho=[]
+valores =[]
+
+carrinho1= ('''
+0 - Nada,
+1 - livro,
+2 - tablet,
+3 - fones de ouvido
+     
+''')
+
+print (' Bem vindo a suas compras On-line')
+print (' Escolha nossos produtos')
+print (produtos)
+print (carrinho1)
+print ('Escolha dentre essas opções')
+escolha1 = int(input())
+escolha2 = int(input())
+escolha3 = int(input())
+
+produtos_lista = ["Nada", "Livro", "Tablet", "Fones de Ouvido"]
+
+produto1 = produtos_lista[escolha1]
+produto2 = produtos_lista[escolha2]
+produto3 = produtos_lista[escolha3]
+
+carrinho = [produto1, produto2, produto3]
+valores = [produtos[produto1], produtos[produto2], produtos[produto3]]
+SOMA =  sum(valores)
+ 
+print(f'''
+.................................
+
+1 - {carrinho[0]} R$ {valores[0]:.2f}
+2 - {carrinho[1]} R$ {valores[1]:.2f}
+3 - {carrinho[2]} R$ {valores[2]:.2f}
+
+.................................
+
+R$ {SOMA:.2f}
+
+VOLTE SEMPRE
+''')
+
